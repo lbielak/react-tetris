825e62a1b2359967b1c015bf36e03e5592f0c359
lbielak <56386312+lbielak@users.noreply.github.com>
2021-09-11 00:23:50 +0200

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,7 +1,6 @@
-var expect = require('expect');
 describe('#test()', function() {
   it('unknown test that pass every time', function()
      {
-      expect(2).to.equal(2);
+      return {message: () => `passed`, pass: true,};
   })
 })
