6ac79c8e393e49456543cd179021751f21e4975c
lbielak <56386312+lbielak@users.noreply.github.com>
2021-09-11 23:35:05 +0200

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 describe('#test()', function() {
   it('unknown test that pass every time', function()
      {
-      return {message: () => `passed`, pass: true,};
+      return {message: () => `passed`, pass: false,};
   })
 })
